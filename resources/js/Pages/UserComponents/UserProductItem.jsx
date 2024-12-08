a71569a0ceb750a9a8314d58d1699fa4092e9612
Ivanblancs <77132176+Ivanblancs@users.noreply.github.com>
2024-12-08 15:34:16 +0530

    Update UserProductItem.jsx

--- resources/js/Pages/UserComponents/UserProductItem.jsx
+++ resources/js/Pages/UserComponents/UserProductItem.jsx
@@ -1,10 +1,11 @@
 import PrimaryButton from '@/Components/PrimaryButton';
+
 const UserProductItem = ({ product, onAddToCart }) => {
     // Ensure price is treated as a number
     const price = Number(product.price);
 
     return (
-        <div className="border p-4 rounded shadow">
+        <div className="border p-4 rounded shadow text-center">
             <h2 className="text-lg font-bold">{product.product_name}</h2>
             <p className="text-gray-600">{product.description}</p>
             <p className="text-blue-500 font-semibold">${price.toFixed(2)}</p>
@@ -15,9 +16,11 @@ const UserProductItem = ({ product, onAddToCart }) => {
                 </span>
             </p>
             {product.available_quantity > 0 && (
-                 <PrimaryButton className="me-2" onClick={() => onAddToCart(product)}>
-                 Add to Cart
-             </PrimaryButton>
+                <div className="d-flex justify-content-center mt-3">
+                    <PrimaryButton onClick={() => onAddToCart(product)}>
+                        Add to Cart
+                    </PrimaryButton>
+                </div>
             )}
         </div>
     );
